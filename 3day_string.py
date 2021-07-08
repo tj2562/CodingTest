0ab19c3d2e9bcedf5969f8f7fb3607bcdb04a539
tj2562 <70683231+tj2562@users.noreply.github.com>
2021-07-09 01:08:02 +0900

    3day

--- 3day_string.py
+++ 3day_string.py
@@ -22,7 +22,7 @@ ans = [S.index(x) if x in S else -1 for x in lower_case]
 print(" ".join(list(map(str, ans))))
 '''
 #2675
-t = int(input())
+'''t = int(input())
 for i in range(t):
     seq, s = input().split()
     text = ''
@@ -30,4 +30,24 @@ for i in range(t):
         for k in range(int(seq)):
             text += (j)
     print(text)
-    text = ''
+    text = '''
+#1157
+'''def most_frequent(data):
+    cnt = []
+    text = list(set(data))
+    for x in text:
+        cnt.append(data.count(x))
+    return text[cnt.index(max(cnt))] if cnt.count(max(cnt)) == 1 else '?'
+
+t = list(map(str, input().upper()))
+print(most_frequent(t))'''
+#1152
+'''
+input_text = list(input().split())
+print(len(input_text))'''
+#2908
+'''input_text = input()
+rvs_text = list(map(int, ''.join(reversed(input_text)).split()))
+print(max(rvs_text))'''
+#
+
