2eb6e34a7c6c668475238af7194b52ada450578c
tj2562 <70683231+tj2562@users.noreply.github.com>
2021-07-18 21:09:20 +0900

    Greedy update

--- Algorithm/1_Greedy.py
+++ Algorithm/1_Greedy.py
@@ -11,3 +11,41 @@ print(count)
 '''
 
 #3-2 그리디 큰 수의 법칙
+'''def calMax(n, input_list):
+    ans = 0
+    count = 0
+
+    if input_list.count(max(input_list)) > 1:
+        return M * max(input_list)
+
+    input_list.sort()
+    max_num = input_list[n-1]
+    max2_num = input_list[n-2]
+
+    for i in range(M):
+        if count != K:
+            ans += max_num
+            count += 1
+        else:
+             ans += max2_num
+             count = 0
+    return ans
+
+N, M, K = map(int, input().split())
+input_list = list(map(int, input().split()))
+print(calMax(N, input_list))'''
+
+#3-3 숫자 카드게임
+'''N, M = map(int, input().split())
+mat = []
+for i in range(N):
+    mat.append(list(map(int, input().split())))
+
+'''
+# 3-4 1이 될 때까지
+'''n, k = map(int, input().split())
+count = 0
+while(n != 1):
+    n = n/k if n/k == int(n/k) else n-1
+    count +=1
+print(count)'''
